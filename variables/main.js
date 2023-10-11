f5ff28e09ddeaed206cc68bc4ce7c7db829f359e
Arsen0312 <greenchay16@gmail.com>
2023-10-11 17:13:40 +0600

    ol

---
+++ variables/main.js
@@ -0,0 +1,16 @@
+let name = 'Arsen'
+let age = 17 
+let city = 'Bishkek'
+
+console.log(name, age, city)
+
+let int = 0
+let color = 'black'
+
+console.log(typeof int, typeof color)
+
+let firstWord = 'live'
+let secondWord = 'print'
+
+console.log(firstWord + secondWord)
+
